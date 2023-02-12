8110afe385530f1e24dbc12572468a43743bcfa3
PeymanPP5530 <5573284@studenti.unige.it>
2023-02-12 18:09:47 +0100

    first commit

--- src/processA.c
+++ src/processA.c
@@ -1,12 +1,67 @@
 #include "./../include/processA_utilities.h"
+#include <bmpfile.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+
+
+
+
+/* the size (in bytes) of shared memory object */
+const int SIZE = 1600*600*sizeof(int);
+
+  bmpfile_t *bmp;
+  
+  rgb_pixel_t pixel = {80, 0, 0, 0};
+
+  	/* name of the shared memory object */
+	const char* name = "OS";
+
+	// /* strings written to shared memory */
+	// const char* message_0 = "Hello";
+	// const char* message_1 = "World!";
+
+	/* shared memory file descriptor */
+	int shm_fd;
+
+	/* pointer to shared memory object */
+	int* ptr;
+
+bool print_flag = false;
+
+
+
+  int width = 1600;
+  int height = 600;
+  int depth = 4;
+  void bitmap_creat(int circle_x, int circle_y);
+  void update_shared_memory( bmpfile_t *bmp);
 
 int main(int argc, char *argv[])
 {
+
+	/* create the shared memory object */
+	shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+
+	/* configure the size of the shared memory object */
+	ftruncate(shm_fd, SIZE);
+
+	/* memory map the shared memory object */
+	ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+
     // Utility variable to avoid trigger resize event on launch
     int first_resize = TRUE;
 
     // Initialize UI
     init_console_ui();
+    bitmap_creat(circle.x, circle.y);
 
     // Infinite loop
     while (TRUE)
@@ -28,6 +83,9 @@ int main(int argc, char *argv[])
         else if(cmd == KEY_MOUSE) {
             if(getmouse(&event) == OK) {
                 if(check_button_pressed(print_btn, &event)) {
+                    //bitmap_creat(circle.x, circle.y);
+                    print_flag = true;
+                    bitmap_creat(circle.x, circle.y);
                     mvprintw(LINES - 1, 1, "Print button pressed");
                     refresh();
                     sleep(1);
@@ -42,9 +100,59 @@ int main(int argc, char *argv[])
         else if(cmd == KEY_LEFT || cmd == KEY_RIGHT || cmd == KEY_UP || cmd == KEY_DOWN) {
             move_circle(cmd);
             draw_circle();
+           bitmap_creat(circle.x, circle.y);
         }
     }
     
     endwin();
     return 0;
+}
+
+
+void update_shared_memory( bmpfile_t *bmp)
+{
+
+    for(int x = 0; x < 1600; x++)
+    {
+        for(int y = 0; y < 600; y++)
+        {
+         rgb_pixel_t *pixel=bmp_get_pixel(bmp ,x, y);
+         ptr[x*599+y]= pixel->blue;
+
+        }
+    }
+
+// rgb_pixel_t *pixel=bmp_get_pixel(bmp ,20, 20);
+//  mvprintw(LINES - 1, 1, "blue=%d , red=%d , green=%d, alpha=%d",pixel->blue,pixel->red,pixel->green,pixel->alpha);
+
+// sleep(5);
+}
+
+
+void bitmap_creat(int circle_x, int circle_y)
+{
+    bmp = bmp_create(width, height, depth);
+    int radius =20;
+    rgb_pixel_t pixel = {80, 0, 0, 0};
+     for(int x = -radius; x <= radius; x++) 
+     {
+      for(int y = -radius; y <= radius; y++) 
+      {
+       // If distance is smaller, point is within the circle
+         if(sqrt(x*x + y*y) < radius) {
+          /*
+          * Color the pixel at the specified (x,y) position
+          * with the given pixel values
+          */
+          bmp_set_pixel(bmp, (circle_x)*(width/(COLS - BTN_SIZE_X -2)) + x, (circle_y)*(height/LINES) + y, pixel);
+         }
+       }
+      }
+if(print_flag == true)
+ {
+     bmp_save(bmp,"testpp" );
+  print_flag = false;
+ }
+  update_shared_memory( bmp);
+  bmp_destroy(bmp);
 }
--- src/processB.c
+++ src/processB.c
@@ -1,7 +1,52 @@
 #include "./../include/processB_utilities.h"
 
+#include <bmpfile.h>
+/*
+In this project we are going to use the bmpfile library to read a bitmap image.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+const int SIZE = 1600*600*sizeof(int);
+
+  bmpfile_t *bmp;
+
+    int center_x;
+    int center_y;
+  
+
+/* name of the shared memory object */
+const char* name = "OS";
+// /* strings written to shared memory */
+// const char* message_0 = "Hello";
+// const char* message_1 = "World!";
+/* shared memory file descriptor */
+int shm_fd;
+/* pointer to shared memory object */
+int* ptr;
+
+// uint32_t bmp_get_width(bmpfile_t *bmp);
+// uint32_t bmp_get_height(bmpfile_t *bmp);
+// uint32_t bmp_get_depth(bmpfile_t *bmp);
+
+void load_shared_memory(void);
+void find_center(void);
+
 int main(int argc, char const *argv[])
 {
+
+    sleep(1);
+    /* open the shared memory object */
+    shm_fd = shm_open(name, O_RDONLY, 0666);
+ 
+    /* memory map the shared memory object */
+    ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0);
+    
     // Utility variable to avoid trigger resize event on launch
     int first_resize = TRUE;
 
@@ -10,6 +55,9 @@ int main(int argc, char const *argv[])
 
     // Infinite loop
     while (TRUE) {
+
+
+
 
         // Get input in non-blocking mode
         int cmd = getch();
@@ -25,11 +73,95 @@ int main(int argc, char const *argv[])
         }
 
         else {
-            mvaddch(LINES/2, COLS/2, '0');
-            refresh();
+
+                load_shared_memory();
+                find_center();
+               bmp_destroy(bmp);
+
+
         }
     }
 
     endwin();
     return 0;
+}
+
+
+
+void find_center(void){
+
+    int y_min=600;
+    int y_max=0;
+    int x_min=1600;
+    int x_max=0;    
+    int center_x;
+    int center_y;
+    for(int x=0;x<1600;x++)
+    	{
+    	  for(int y=0;y<600;y++)
+    		{
+
+                rgb_pixel_t *pixel = bmp_get_pixel(bmp ,x, y);
+    			if(pixel->blue == 80)
+    			{
+    				if(x<x_min) {x_min=x;};
+
+    				if(x>x_max) {x_max=x;};
+
+    				if(y<y_min) {y_min=y;};
+
+    				if(y>y_max) {y_max=y;};
+    
+    			}
+
+    		}
+
+
+
+    	}
+    center_x=(x_min+x_max)/2;
+    center_y=(y_min+y_max)/2;
+
+    center_x=center_x/(1600/COLS);
+    center_y=center_y/(600/LINES);
+
+    mvprintw(LINES - 1, 1, "center_x: %d  ", center_x);
+    mvprintw(LINES - 2, 1, "center_y: %d  ", center_y);
+
+
+            mvaddch(center_y, center_x, '0');
+            refresh();
+
+                    refresh();
+
+
+}
+
+
+void load_shared_memory(void){
+
+    bmp = bmp_create(1600, 600, 4);
+
+    for(int x = 0; x < 1600; x++)
+    {
+        for(int y = 0; y < 600; y++)
+        {
+
+            if (ptr[x*599+y]==80)
+            {
+             rgb_pixel_t pixel = {ptr[x*599+y], 0, 0, 0};
+             bmp_set_pixel(bmp,x,y, pixel);
+            }
+            else{
+            rgb_pixel_t pixel = {255, 255, 255, 0};
+            bmp_set_pixel(bmp,x,y, pixel);
+            }
+            
+
+        }
+    }
+
+ // bmp_save(bmp,"processBB" );
+
+
 }
